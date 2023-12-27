bcde43e5c76d945518da68ac6d2248f642bfa990
mantesh-ops <95992777+mantesh-ops@users.noreply.github.com>
2023-12-27 15:15:00 +0530

    Add files via upload

---
+++ process_feedback.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $lastName = $_POST["last-name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Validate and sanitize input (you can add more validation as needed)
+
+    // Save data to a file
+    $data = "Name: $name\nLast Name: $lastName\nEmail: $email\nMessage: $message\n\n";
+    file_put_contents("feedback_data.txt", $data, FILE_APPEND);
+
+    // You can also store data in a database instead of a file if needed
+
+    echo "Feedback submitted successfully!";
+} else {
+    echo "Invalid request method!";
+}
